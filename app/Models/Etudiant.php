b3d49258eeb2f7cc611099499995a0826b53c976
Minela <Minelatounky@gmail.com>
2026-03-05 01:47:45 +0000

    Model

---
+++ app/Models/Etudiant.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Etudiant extends Model
+{
+    protected $fillable = ['prenom','nom','email','date_naissance'];
+    public function cours(){
+    return $this->belongsToMany(Cours::class);
+}   
+}
